034fde479902acece85e8dd829739ed65daa55f3
Bingchen Liu <bingchen.liu@mail.utoronto.ca>
2021-10-11 17:17:06 -0400

    finish class Product

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -2,4 +2,16 @@
  * Products name, id and types will store in here.
  */
 public class Product {
+    public String name;
+    public int u_id;
+    public float price;
+    public int stock;
+
+    public Product(String name, int u_id, float price, int stock){
+        this.name = name;
+        this.u_id = u_id;
+        this.price = price;
+        this.stock = stock;
+    }
+
 }
