e1af609d83bf218ffda3f677d2f0e5c64b42b28b
Bingchen Liu <bingchen.liu@mail.utoronto.ca>
2021-10-11 19:53:31 -0400

    collating documents

--- src/Entity/Order.java
+++ src/Entity/Order.java
@@ -1,10 +1,13 @@
+package Entity;
+
 import java.util.List;
+
 /**
  * This is a controller to our ordering process.
  */
 public class Order {
     private int o_id;
-    private List<Product>  list_products;
+    private List<Product> list_products;
     private String status;
 
     //Initialize a order
