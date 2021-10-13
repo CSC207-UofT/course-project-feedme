7d7969bb581b4649da70480045510a3a2e13beaa
Jing Liu <jingl.liu@mail.utoronto.ca>
2021-10-13 14:03:04 -0400

    Split child classes in User into separate files

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -23,32 +23,6 @@ abstract class User {
     public char getUserType_() { return this.type_; }
 }
 
-class Customer extends User{
-    private String address;
-    public Customer(String name, String address, int id, int phone_num, int password, char type_){
-        super(name, id, phone_num, password, type_);
-        this.address = address;
-    }
 
-    public String getCustomerAddress() { return this.address; }
-}
 
-class Restaurant extends User{
-    private String address;
-    private List<Product> list_products;
-    public Restaurant(String name, String address, int id, int phone_num, int password, char type_){
-        super(name, id, phone_num, password, type_);
-        this.address = address;
-        this.list_products = new ArrayList<Product>();
-    }
-    public String getRestaurantAddress() { return this.address; }
-    public List<Product> getRestaurantProducts() { return this.list_products; }
-    public void addRestaurantProducts(Product p) { this.list_products.add(p); }
-    public void removeRestaurantProducts(Product p) {this.list_products.remove(p); }
-}
 
-class DeliveryPerson extends User{
-    public DeliveryPerson(String name, int id, int phone_num, int password, char type_){
-        super(name, id, phone_num, password, type_);
-    }
-}
