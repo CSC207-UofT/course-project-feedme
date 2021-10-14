e4b419e0b42d6b913669d3c71a8547b3b32bcef8
Jing Liu <jingl.liu@mail.utoronto.ca>
2021-10-14 12:21:49 -0400

    Edit naming format in Usermanager

--- src/main/java/UseCases/Usermanager.java
+++ src/main/java/UseCases/Usermanager.java
@@ -7,33 +7,33 @@ import Entity.User;
 import java.util.HashMap;
 
 public class Usermanager {
-    private HashMap<String, User> user_map;
+    private HashMap<String, User> userHashMap;
 
     public Usermanager(){
-        this.user_map = new HashMap<String, User>();
+        this.userHashMap = new HashMap<String, User>();
     }
 
-    public boolean add_user(String phone_num, User user){
-        if(!this.user_map.containsKey(phone_num)){
-            this.user_map.put(phone_num, user);
+    public boolean addUser(String phone_num, User user){
+        if(!this.userHashMap.containsKey(phone_num)){
+            this.userHashMap.put(phone_num, user);
             return true;
         }
         return false;
     }
 
-    public boolean user_lookup(String phone_num) { return this.user_map.containsKey(phone_num); }
+    public boolean userLookup(String phone_num) { return this.userHashMap.containsKey(phone_num); }
 
-    public boolean check_password(String phone_num, String enter_password) {
-        return (this.user_map.get(phone_num).getUserPassword().equals(enter_password));
+    public boolean checkPassword(String phone_num, String enter_password) {
+        return (this.userHashMap.get(phone_num).getUserPassword().equals(enter_password));
     }
 
-    public Customer customer_signup(String name, String phone_num, String password, char type_, String address){
-        return new Customer(name, phone_num, password, type_, address);
+    public Customer customerSignup(String name, String phone_num, String password, char type_, String address){
+        return new Customer(name, address, phone_num, password, type_);
     }
-    public Restaurant restaurant_signup(String name, String phone_num, String password, char type_, String address){
-        return new Restaurant(name, phone_num, password, type_, address);
+    public Restaurant restaurantSignup(String name, String phone_num, String password, char type_, String address){
+        return new Restaurant(name, address, phone_num, password, type_);
     }
-    public DeliveryPerson deliveryperson_signup(String name, String phone_num, String password, char type_){
+    public DeliveryPerson deliveryPersonSignup(String name, String phone_num, String password, char type_){
         return new DeliveryPerson(name, phone_num, password, type_);
     }
 }
