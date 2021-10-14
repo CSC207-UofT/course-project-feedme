a50c0fc2c8f205fd5b96984edaa3ae66e8483c2b
Jing Liu <jingl.liu@mail.utoronto.ca>
2021-10-13 20:18:49 -0400

    Finish UserTest.java and it's working.

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -8,15 +8,31 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class UserTest {
-    User user;
+    User customer;
+    User restaurant;
+    User deliveryperosn;
 
     @Before
     public void setUp() {
-        user = new Customer("Alex", "25 St. George St" ,"4168237263", "asdf1234", 'c');
+        customer = new Customer("Alex", "25 St. George St" ,"4168237263", "asdf1234",'c');
+        restaurant = new Restaurant("Ramen", "7 St Mary St", "4161234554", "sdfa89789", 'r');
+        deliveryperosn = new DeliveryPerson("Jack", "4167892537", "1234567", 'd');
     }
 
     @Test
-    public void TestEmptyConstructor() {
-
+    public void TestCustomerConstructor() {
+        assertEquals("Alex", customer.getUserName());
     }
+
+    @Test
+    public void TestRestaurantConstructor() {
+        assertEquals("4161234554", restaurant.getUserPhone_num());
+    }
+
+    @Test
+    public void TestDeliverypersonConstructor() {
+        assertEquals('d', deliveryperosn.getUserType_());
+    }
+
+
 }
