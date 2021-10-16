902aee63a411c53871c87538e70e40174a44b310
Bingchen Liu <bingchen.liu@mail.utoronto.ca>
2021-10-16 10:19:56 -0400

    Add Javadoc and fix addRestaurantProduct in Restaurant.java

--- src/main/java/Entity/Restaurant.java
+++ src/main/java/Entity/Restaurant.java
@@ -2,40 +2,70 @@ package Entity;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Restaurant is a child class of User. User type 'r' is assigned to Restaurant class.
- */
-
 public class Restaurant extends User {
     private String address;
     private List<Product> productList;
 
-    // Initiate a Restaurant
+
+    /**
+     * Restaurant is a child class of User. User type 'r' is assigned to Restaurant class.
+     * Construct a Restaurant, giving them the name, phone_num, password, type and address.
+     *
+     * @param name        The name of Restaurant
+     * @param phone_num   The phone number of Restaurant
+     * @param password    The password of Restaurant
+     * @param type_       The type of User
+     * @param address     The delivery address of Restaurant
+     */
     public Restaurant(String name, String address, String phone_num, String password, char type_){
         super(name, phone_num, password, type_);
         this.address = address;
         this.productList = new ArrayList<Product>();
     }
 
+
+    /**
+     * Get Restaurant's address.
+     *
+     * @return a string of the address.
+     */
     public String getRestaurantAddress() { return this.address; }
 
+
+    /**
+     * Get Restaurant's product.
+     *
+     * @return a list of Product.
+     */
     public List<Product> getRestaurantProducts() { return this.productList; }
 
-    public boolean addRestaurantProduct(Product p) {
-        if (this.productList.contains(p)) {
+
+    /**
+     * Attempt to add new Product to the Restaurant's productList, if the product
+     * hasn't added in the productList. Otherwise, return False.
+     *
+     * @return True if Product is added in the productList successfully.
+     */
+    public boolean addRestaurantProduct(Product product) {
+        if (this.productList.contains(product)) {
             return false;
-        } else {
-            this.productList.add(p);
-            return true;
         }
+        this.productList.add(product);
+        return true;
     }
 
-    public boolean removeRestaurantProduct(Product p) {
-            if (!this.productList.contains(p)) {
-                return false;
-            } else {
-                this.productList.remove(p);
-                return true;
-            }
+
+    /**
+     * Attempt to remove an exiting Product from the Restaurant's productList, if the product
+     * is in the productList. Otherwise, return False.
+     *
+     * @return True if Product removes from the productList successfully.
+     */
+    public boolean removeRestaurantProduct(Product product) {
+        if (!this.productList.contains(product)) {
+            return false;
+        }
+        this.productList.remove(product);
+        return true;
     }
 }
