37ff0bfcc7c83c75f3cdedff0d13eb4dc71da668
wanedwar <edward.wan@mail.utoronto.ca>
2021-10-15 21:42:48 -0400

    walkthrough uploaded

--- src/main/java/DeliveryPerson.java
+++
@@ -1,7 +0,0 @@
-import Entity.User;
-
-class DeliveryPerson extends User {
-    public DeliveryPerson(String name, int id, String phone_num, String password, char type_) {
-        super(name, phone_num, password, type_);
-    }
-}
