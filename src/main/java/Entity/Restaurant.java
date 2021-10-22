23718e8892cbde816d849372796d905dddecee94
Jing Liu <jingl.liu@mail.utoronto.ca>
2021-10-21 21:39:07 -0400

    Rename listProducts to menu in restaurant

--- src/main/java/Entity/Restaurant.java
+++ src/main/java/Entity/Restaurant.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Restaurant extends User {
     private final String address;
-    private List<Product> productList;  //Since restaurant can add and remove products from productList, the field may
+    private List<Product> menu;  //Since restaurant can add and remove products from productList, the field may
                                         //not be final.
 
 
@@ -21,7 +21,7 @@ public class Restaurant extends User {
     public Restaurant(String name, String address, String phone_num, String password, char type_){
         super(name, phone_num, password, type_);
         this.address = address;
-        this.productList = new ArrayList<>();
+        this.menu = new ArrayList<>();
     }
 
 
@@ -38,7 +38,7 @@ public class Restaurant extends User {
      *
      * @return a list of Product.
      */
-    public List<Product> getRestaurantProducts() { return this.productList; }
+    public List<Product> getRestaurantProducts() { return this.menu; }
 
 
     /**
@@ -47,11 +47,11 @@ public class Restaurant extends User {
      *
      * @return True if Product is added in the productList successfully.
      */
-    public boolean addRestaurantProduct(Product product) {
-        if (this.productList.contains(product)) {
+    public boolean addProductToMenu(Product product) {
+        if (this.menu.contains(product)) {
             return false;
         }
-        this.productList.add(product);
+        this.menu.add(product);
         return true;
     }
 
@@ -62,11 +62,11 @@ public class Restaurant extends User {
      *
      * @return True if Product removes from the productList successfully.
      */
-    public boolean removeRestaurantProduct(Product product) {
-        if (!this.productList.contains(product)) {
+    public boolean removeProductFromMenu(Product product) {
+        if (!this.menu.contains(product)) {
             return false;
         }
-        this.productList.remove(product);
+        this.menu.remove(product);
         return true;
     }
 }
