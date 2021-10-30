e846753a92ce768de63c1e3b01881a1d2aa977ab
francisliang <francis.liang@mail.utoronto.ca>
2021-10-30 15:34:35 -0400

    resController

--- src/main/java/UseCases/RestaurantManager.java
+++ src/main/java/UseCases/RestaurantManager.java
@@ -3,35 +3,38 @@ package UseCases;
 import Entity.Product;
 import Entity.Restaurant;
 import Entity.Order;
-import UseCases.OrderManager;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class RestaurantManager {
     Restaurant restaurant;
-    OrderManager o_manager;
+    ArrayList<Order> orderHistory;
     public RestaurantManager(Restaurant restaurant){
         this.restaurant = restaurant;
-        this.o_manager = new OrderManager();}
+        this.orderHistory = new ArrayList<Order>();
+    }
 
     //Add product to restaurant menu if boolean is true, vice versa.
-    public void editRestaurantMenu(Product item, boolean bool){
+    public boolean editRestaurantMenu(Product item, boolean bool){
         if(bool){
-            this.restaurant.addRestaurantProduct(item);
+            return this.restaurant.addRestaurantProduct(item);
         }
-        this.restaurant.removeRestaurantProduct(item);
+        return this.restaurant.removeRestaurantProduct(item);
     }
 
     //Change restaurant menu to a new one.
-    public void changeMenu(List<Product> menu){
-        this.restaurant.setProductList(menu);
+    public List<Product> getMenu(){
+        return this.restaurant.getRestaurantProducts();
     }
+
 
     //Precondition: products in order are all from the restaurant.
     //Receive order and record it. The stock reduces by given quantities in the order.
     public void receiveOrder(Order order){
-        this.o_manager.addOrder(order);
+        this.orderHistory.add(order);
         HashMap<Product, Integer> hashMap = order.getOrderProducts();
         for (Product key: hashMap.keySet()) {
             key.reduceProductStock(hashMap.get(key));
