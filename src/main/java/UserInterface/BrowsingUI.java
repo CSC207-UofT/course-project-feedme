59c2dbeb44ac0ff8309a47ca1135e6c2517c6e85
Jing Liu <jingl.liu@mail.utoronto.ca>
2021-11-14 17:24:23 -0500

    Add BrowsingUI

---
+++ src/main/java/UserInterface/BrowsingUI.java
@@ -0,0 +1,20 @@
+package UserInterface;
+
+import java.util.Scanner;
+import Controller.BrowsingController;
+
+/**
+ * An user interface for customer to view restaurants' menu and select which restaurant to order.
+ */
+
+public class BrowsingUI {
+
+    public void viewRestaurants(BrowsingController controller) {
+
+    }
+
+}
+
+
+
+
