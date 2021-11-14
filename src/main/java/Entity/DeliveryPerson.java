e0c4151724416790b963a32b57f25c52380af8a3
jenny16888 <76978696+jenny16888@users.noreply.github.com>
2021-11-14 17:27:38 -0500

    DeliveryController and DeliveryManager

--- src/main/java/Entity/DeliveryPerson.java
+++ src/main/java/Entity/DeliveryPerson.java
@@ -1,8 +1,28 @@
 package Entity;
 
+import java.util.Set;
+
 public class DeliveryPerson extends User {
 
 
+    private Order currentOrder;
+    private Set orderHistory;
+
+    public Order getCurrentOrder() {
+        return currentOrder;
+    }
+
+    public void setCurrentOrder(Order currentOrder) {
+        this.currentOrder = currentOrder;
+    }
+
+    public Set getOrderHistory() {
+        return orderHistory;
+    }
+
+    public void setOrderHistory(Set orderHistory) {
+        this.orderHistory = orderHistory;
+    }
     /**
      * DeliveryPerson is a child class of User. User type 'd' is assigned to DeliveryPerson class.
      * Construct a DeliveryPerson, giving the name, phone_num, password, and type.
