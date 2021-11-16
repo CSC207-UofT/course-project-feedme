8be86578ff41cf9dec3fbfa1edde28f58af5089b
Edward <edwardkinsengwan@gmail.com>
2021-11-15 22:38:08 -0500

    User/Restaurant gatherer/writer test

---
+++ src/test/java/UserGathererTest.java
@@ -0,0 +1,29 @@
+import Entity.User;
+import UseCases.UserGatherer;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class UserGathererTest {
+    UserGatherer ug = new UserGatherer();
+    HashMap<String, User> map;
+    @Before
+    public void setup(){
+        map = ug.loadUser();
+    }
+
+    @Test
+    public void testUserType(){
+        assertEquals(map.get("416").getUserType(), "c");
+    }
+
+    @Test
+    public void testUserName(){
+        assertEquals(map.get("416").getUserName(), "pauline");
+    }
+
+}
