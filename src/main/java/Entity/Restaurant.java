5bbbd0b51a5429ebcf202e5afd3357b28ec4e67a
Bingchen Liu <bingchen.liu@mail.utoronto.ca>
2021-11-15 20:48:55 -0500

    Update order of constructor and test cases

--- src/main/java/Entity/Restaurant.java
+++ src/main/java/Entity/Restaurant.java
@@ -1,8 +1,12 @@
 package Entity;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Restaurant extends User {
+public class Restaurant extends User implements Serializable {
+
+    private static final long serialVersionUID = -2191481848339591142L;
+
     private final String address;
     private final List<Product> menu;  //Since restaurant can add and remove products from productList, the field may
     //not be final.
@@ -17,17 +21,19 @@ public class Restaurant extends User {
      * @param password    The password of Restaurant
      * @param type_       The type of User
      * @param address     The delivery address of Restaurant
-     */public Restaurant(String name, String phone_num, String password, String type_, String address){
+     */
+    public Restaurant(String name, String phone_num, String password, String type_, String address){
         super(name, phone_num, password, type_);
         this.address = address;
         this.menu = new ArrayList<>();
     }
 
-    public Restaurant(String name, String address, String phone_num, String password){
-        super(name, phone_num, password, "r");
+    public Restaurant(String name, String phone_num, String password, String type_, String address, List<Product> menu){
+        super(name, phone_num, password, type_);
         this.address = address;
-        this.menu = new ArrayList<>();
+        this.menu = menu;
     }
+
 
     /**
      * Get Restaurant's address.
@@ -72,5 +78,10 @@ public class Restaurant extends User {
         }
         this.menu.remove(product);
         return true;
+    }
+
+    @Override
+    public String toString() {
+        return this.getUserName();
     }
 }
