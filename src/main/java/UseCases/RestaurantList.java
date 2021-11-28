a6283797a50d2e529c1788ea67b5aeaa00991486
Allen Liu <90647481+Allen0429@users.noreply.github.com>
2021-11-27 22:46:12 -0500

    Update and rename restaurantList.java to RestaurantList.java

--- src/main/java/UseCases/RestaurantList.java
+++ src/main/java/UseCases/RestaurantList.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
 
-public class restaurantList {
+public class RestaurantList {
 
 
     RestaurantGatherer restaurantGatherer = new RestaurantGatherer();
