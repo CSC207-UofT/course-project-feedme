f29b6126e2c61962489d5b173709829897696c3a
Bingchen Liu <bingchen.liu@mail.utoronto.ca>
2021-11-27 22:50:09 -0500

    Update test cases

--- src/test/java/UserManagerTest.java
+++ src/test/java/UserManagerTest.java
@@ -1,8 +1,9 @@
 import Entity.Customer;
-import Entity.User;
 import UseCases.UserManager;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.assertTrue;
 
@@ -16,10 +17,13 @@ public class UserManagerTest {
 
     @Test
     public void testCreateUser() {
-        User customer = new Customer("Alex", "4168237263" ,"avg343", "c","11 Wellesley St E");
-        um.createUser("4168237263", customer);
-        assertTrue(um.verifyUser("4168237263", customer.getUserPassword()));
-        assertTrue(um.userLookup("4168237263"));
+        Random rnd = new Random();
+        int phone_number = 1000000000 + rnd.nextInt(900000000);
+        Customer customer = new Customer("Alex", String.valueOf(phone_number) ,"avg343", "c","11 Wellesley St E");
+        assertTrue(um.createCustomer(customer.getUserName(), customer.getUserPhone_num(),
+                customer.getUserPassword(), customer.getUserType(), customer.getCustomerAddress()));
+        assertTrue(um.verifyUser(String.valueOf(phone_number), customer.getUserPassword()));
+        assertTrue(um.userLookup(String.valueOf(phone_number)));
     }
 
 }
