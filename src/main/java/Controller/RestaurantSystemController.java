15ab8156186456ddf1bb6803feb0c08445b04ea7
qlndzt <90402097+qlndzt@users.noreply.github.com>
2021-12-04 23:55:57 -0500

    Update RestaurantSystemController.java

--- src/main/java/Controller/RestaurantSystemController.java
+++ src/main/java/Controller/RestaurantSystemController.java
@@ -3,18 +3,27 @@ package Controller;
 
 import Presenter.RestaurantSystemPresenter;
 import UseCases.RestaurantManager;
+import InOut.SystemInOut;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-public class RestaurantSystemController{
+public class RestaurantSystemController implements SystemInOut{
 
     RestaurantSystemPresenter rp;
 
-    public interface InOut {
-        String getInput() throws IOException;
+    @Override
+    public String getInput() throws IOException {
+        BufferedReader reader;
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
+    }
 
-        void sendOutput(String output);
+    @Override
+    public void sendOutput(String output) {
+        System.out.println(output);
     }
 
     public RestaurantManager resManager;
@@ -31,35 +40,36 @@ public class RestaurantSystemController{
             String command = inout.getInput();
             if(Objects.equals(command, "editMenu")){
                 this.resManager.getMenu();
-                rp.editOrView(true);
+                rp.askId();
                 String numStr = inout.getInput();
 
                 if (this.resManager.containProduct(numStr)) {
-                    rp.editOrView(false);
+                    sendOutput(p.askChange());
                     String type = inout.getInput();
                     if (Objects.equals(type, "1")) {
-                        rp.editOptions(1);
+                        sendOutput(rp.askName());
                         String newName = inout.getInput();
                         this.resManager.editName(numStr, newName);
                     }
 
                     if (Objects.equals(type, "2")) {
-                        rp.editOptions(2);
+                        sendOutput(rp.askStock());
                         String stockString = inout.getInput();
                         int newStock = Integer.parseInt(stockString);
                         this.resManager.editRestaurantMenu(numStr, newStock);
                     }
 
                     if (Objects.equals(type, "3")) {
-                        rp.editOptions(3);
+                        sendOutput((rp.askPeice());
                         double newPrice = Double.parseDouble(inout.getInput());
                         this.resManager.editPrice(numStr, newPrice);
                     } else {
-                        rp.notInMenu();
+                        sendOutput(rp.notInMenu());
+                        sendOutput(rp.askName());
                         String prod_name = inout.getInput();
-                        rp.askNewPrice();
+                        sendOutput(rp.askPrice());
                         String prod_price = inout.getInput();
-                        rp.askNewStock();
+                        sendOutput(rp.askStock());
                         String prod_stock = inout.getInput();
                         this.resManager.createProduct(prod_name, Double.parseDouble(prod_price), Integer.parseInt(prod_stock));
                         break;
