22c25ce360e3f0ec7de1326f54b6d02dcf18b84d
qlndzt <90402097+qlndzt@users.noreply.github.com>
2021-12-05 00:25:55 -0500

    Update RestaurantSystemController.java

--- src/main/java/Controller/RestaurantSystemController.java
+++ src/main/java/Controller/RestaurantSystemController.java
@@ -3,73 +3,64 @@ package Controller;
 
 import Presenter.RestaurantSystemPresenter;
 import UseCases.RestaurantManager;
-import InOut.SystemInOut;
 
 import java.io.IOException;
 import java.util.Objects;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
-public class RestaurantSystemController implements SystemInOut{
+public class RestaurantSystemController{
 
     public RestaurantManager resManager;
     RestaurantSystemPresenter rp;
 
-    @Override
-    public String getInput() throws IOException {
-        BufferedReader reader;
-        reader = new BufferedReader(new InputStreamReader(System.in));
-        return reader.readLine();
-    }
+    public interface InOut {
+        String getInput() throws IOException;
 
-    @Override
-    public void sendOutput(String output) {
-        System.out.println(output);
+        void sendOutput(String output);
     }
-
     public RestaurantSystemController(RestaurantManager resManager) {
         this.resManager = resManager;
     }
-
-    public void run() throws IOException {
+    public void run(InOut inout) throws IOException {
 
         while(true){
 
-            sendOutput(rp.greeting());
-            String command = getInput();
+
+            inout.sendOutput(rp.greeting());
+            String command = inout.getInput();
             if(Objects.equals(command, "editMenu")){
                 this.resManager.getMenu();
-                sendOutput(rp.askId());
-                String numStr = getInput();
+                inout.sendOutput(rp.askId());
+                String numStr = inout.getInput();
 
                 if (this.resManager.containProduct(numStr)) {
-                    sendOutput(rp.askChange());
-                    String type = getInput();
+                    inout.sendOutput(rp.askChange());
+                    String type = inout.getInput();
                     if (Objects.equals(type, "1")) {
-                        sendOutput(rp.askName());
-                        String newName = getInput();
+                        inout.sendOutput(rp.askName());
+                        String newName = inout.getInput();
                         this.resManager.editName(numStr, newName);
                     }
 
                     if (Objects.equals(type, "2")) {
-                        sendOutput(rp.askStock());
-                        String stockString = getInput();
+                        inout.sendOutput(rp.askStock());
+                        String stockString = inout.getInput();
                         int newStock = Integer.parseInt(stockString);
                         this.resManager.editRestaurantMenu(numStr, newStock);
                     }
 
                     if (Objects.equals(type, "3")) {
-                        sendOutput(rp.askPrice());
-                        double newPrice = Double.parseDouble(getInput());
+                        inout.sendOutput(rp.askPrice());
+                        double newPrice = Double.parseDouble(inout.getInput());
                         this.resManager.editPrice(numStr, newPrice);
-                    } else {
-                        sendOutput(rp.notInMenu());
-                        sendOutput(rp.askName());
-                        String prod_name = getInput();
-                        sendOutput(rp.askPrice());
-                        String prod_price = getInput();
-                        sendOutput(rp.askStock());
-                        String prod_stock = getInput();
+                    }
+                    else {
+                        inout.sendOutput(rp.notInMenu());
+                        inout.sendOutput(rp.askName());
+                        String prod_name = inout.getInput();
+                        inout.sendOutput(rp.askPrice());
+                        String prod_price = inout.getInput();
+                        inout.sendOutput(rp.askStock());
+                        String prod_stock = inout.getInput();
                         this.resManager.createProduct(prod_name, Double.parseDouble(prod_price), Integer.parseInt(prod_stock));
                         break;
                     }
@@ -86,7 +77,7 @@ public class RestaurantSystemController implements SystemInOut{
                 }}
         }
         {
-            sendOutput(rp.wrongCommand());
+            inout.sendOutput(rp.wrongCommand());
         }
     }
 }
