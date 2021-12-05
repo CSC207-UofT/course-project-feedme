a9af483552079de821c24a6289f10d8775096fab
Bingchen Liu <bingchen.liu@mail.utoronto.ca>
2021-12-05 13:22:23 -0500

    Update method getOrderPrice and toString in Cart.java

--- src/main/java/Entity/Cart.java
+++ src/main/java/Entity/Cart.java
@@ -1,19 +1,40 @@
 package Entity;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
 
 /**
  * Before an order is generated, item are added to the cart which is then used to generate an Order.
  */
 public class Cart {
-    private final HashMap<Product, Integer> cart; // key is a Product, value is product number
-
+    private final HashMap<Product, Integer> cart; // key is a Product, value is product quantity
     //Initialize an chart
     public Cart(){
         this.cart = new HashMap<>();
     }
 
     public HashMap<Product, Integer> getCart() { return this.cart; }
+
+    public boolean addProductToCart(Product product, Integer quantity) {
+        if (this.cart.containsKey(product)) {
+            if (product.getProductStock() >= quantity) {// Check if there is enough stock
+                cart.put(product, cart.get(product) + quantity);
+                product.updateStock(-quantity);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (product.getProductStock() >= quantity) {// Check if there is enough stock
+                cart.put(product, quantity);
+                product.updateStock(quantity);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
     public boolean removeProductFromCart(Product product, Integer quantity) {
         if (cart.containsKey(product)) {
@@ -35,24 +56,24 @@ public class Cart {
         }
     }
 
-    public double getOrderPrice(){
+    public double getOrderPrice() {
         double order_price = 0.00;
-        Integer quantity;
-        for(Product product: cart.keySet()){
+        int quantity;
+        for (Product product : cart.keySet()) {
             quantity = cart.get(product);
-            order_price += product.getProductPrice() * quantity ;
+            order_price += product.getProductPrice() * quantity;
         }
-        return order_price;
+        BigDecimal b = new BigDecimal(order_price);
+        return b.setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
 
     @Override
     public String toString() {
         StringBuilder items = new StringBuilder("Here are the items in your cart:\n");
         for (Product product: this.cart.keySet()) {
-            items.append(this.cart.get(product)).append("\t").append(product);
+            items.append(this.cart.get(product)).append(" ").append(product).append("\n");
         }
         items.append("\nTotal price: $").append(getOrderPrice());
         return items.toString();
     }
-
 }
