2d46ee2df0c178198a4f10e5378a8ea0232b7da1
qlndzt <90402097+qlndzt@users.noreply.github.com>
2021-12-05 00:10:14 -0500

    Update RestaurantSystemController.java

--- src/main/java/Controller/RestaurantSystemController.java
+++ src/main/java/Controller/RestaurantSystemController.java
@@ -12,6 +12,7 @@ import java.io.InputStreamReader;
 
 public class RestaurantSystemController implements SystemInOut{
 
+    public RestaurantManager resManager;
     RestaurantSystemPresenter rp;
 
     @Override
@@ -26,51 +27,49 @@ public class RestaurantSystemController implements SystemInOut{
         System.out.println(output);
     }
 
-    public RestaurantManager resManager;
-
     public RestaurantSystemController(RestaurantManager resManager) {
         this.resManager = resManager;
     }
 
-    public void run(InOut inout) throws IOException {
+    public void run() throws IOException {
 
         while(true){
 
-            rp.greeting();
-            String command = inout.getInput();
+            sendOutput(rp.greeting());
+            String command = getInput();
             if(Objects.equals(command, "editMenu")){
                 this.resManager.getMenu();
-                rp.askId();
-                String numStr = inout.getInput();
+                sendOutput(rp.askId());
+                String numStr = getInput();
 
                 if (this.resManager.containProduct(numStr)) {
-                    sendOutput(p.askChange());
-                    String type = inout.getInput();
+                    sendOutput(rp.askChange());
+                    String type = getInput();
                     if (Objects.equals(type, "1")) {
                         sendOutput(rp.askName());
-                        String newName = inout.getInput();
+                        String newName = getInput();
                         this.resManager.editName(numStr, newName);
                     }
 
                     if (Objects.equals(type, "2")) {
                         sendOutput(rp.askStock());
-                        String stockString = inout.getInput();
+                        String stockString = getInput();
                         int newStock = Integer.parseInt(stockString);
                         this.resManager.editRestaurantMenu(numStr, newStock);
                     }
 
                     if (Objects.equals(type, "3")) {
-                        sendOutput((rp.askPeice());
-                        double newPrice = Double.parseDouble(inout.getInput());
+                        sendOutput((rp.askPrice());
+                        double newPrice = Double.parseDouble(getInput());
                         this.resManager.editPrice(numStr, newPrice);
                     } else {
                         sendOutput(rp.notInMenu());
                         sendOutput(rp.askName());
-                        String prod_name = inout.getInput();
+                        String prod_name = getInput();
                         sendOutput(rp.askPrice());
-                        String prod_price = inout.getInput();
+                        String prod_price = getInput();
                         sendOutput(rp.askStock());
-                        String prod_stock = inout.getInput();
+                        String prod_stock = getInput();
                         this.resManager.createProduct(prod_name, Double.parseDouble(prod_price), Integer.parseInt(prod_stock));
                         break;
                     }
@@ -87,7 +86,7 @@ public class RestaurantSystemController implements SystemInOut{
                 }}
         }
         {
-            rp.wrongCommand();
+            sendOutput(rp.wrongCommand());
         }
     }
 }
