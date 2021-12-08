566857b673db7eb0144c072f6256ab95298237db
llc <ll.cheng@mail.utoronto.ca>
2021-12-08 07:58:50 -0500

    restaurantSystemController

--- src/main/java/InOut/SystemInOut.java
+++ src/main/java/InOut/SystemInOut.java
@@ -1,35 +1,15 @@
 package InOut;
-
-import Controller.BrowsingController;
-import Controller.LoginController;
-import Controller.OrderController;
-import Controller.SignupController;
-import Controller.RestaurantSystemController;
-
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
-
-public class SystemInOut implements BrowsingController.InOut, OrderController.InOut
-        , LoginController.InOut, SignupController.InOut, RestaurantSystemController.InOut {
+public interface SystemInOut {
 
     /**
      * Input and output boundary that implements InOut in each controller. SystemInOut collect client's input from UI
      * and sent input to controller. It also returns desired output from controller to UI.
      */
-    BufferedReader reader;
-
-    public SystemInOut() { reader = new BufferedReader(new InputStreamReader(System.in));}
 
     // Get client's input form UI and sent to controller.
-    @Override
-    public String getInput() throws IOException {
-        return reader.readLine();
-    }
+     String getInput() throws IOException;
     // Return desired output from controller to UI.
-    @Override
-    public void sendOutput(String output) {
-        System.out.println(output);
-    }
+     void sendOutput(String output);
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,3 @@
-//import Entity.Customer;
-//import UseCases.UserReadWrite;
-//import UserInterface.BrowsingUI;
-//import UserInterface.LoginUI;
-//import UserInterface.CartUI;
-//import UserInterface.OrderUI;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Map;
-//
-//
-//public class Main {
-//
-//    public static void main(String[] args) {
-//        LoginUI Lui = new LoginUI();
-//        List<String> user = Lui.login();
-//        if (user.get(1).equals("c")) {
-//            String customerNum = user.get(0);
-//            BrowsingUI Bui = new BrowsingUI();
-//            String restaurantNum = Bui.browsing();
-//            try {
-//                CartUI Cui = new CartUI(restaurantNum);
-//                Map<String, Integer> cart = Cui.ordering();
-//                OrderUI Oui = new OrderUI(customerNum, restaurantNum, cart);
-//                Oui.placeOrder();
-//            } catch (NullPointerException ignored) {
-//            }
-//        }
-//    }
-//
-//}
-
 import Entity.Cart;
 import Entity.Customer;
 import Entity.Restaurant;
